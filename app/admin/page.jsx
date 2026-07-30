e1d6a80f1a6886f25a96ac7e44ee76002aa8172a
Michael Turner <michael@example.com>
2026-07-30 19:26:19 -0400

    Remove stray lockfile

--- app/admin/page.jsx
+++
@@ -1,9 +0,0 @@
-import AdminHome from "@/components/admin/dashboard/AdminHome";
-
-export default async function Page() {
-  return (
-    <div className="p-8">
-      <AdminHome />
-    </div>
-  );
-}
